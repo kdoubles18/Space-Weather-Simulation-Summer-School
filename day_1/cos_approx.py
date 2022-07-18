792d78ba409557a92f06417324bf066fdbdc2ade
Kaitlin Doublestein <kdoubles@umich.edu>
2022-07-18 16:19:26 -0600

    Day 1 Project

--- day_1/cos_approx.py
+++ day_1/cos_approx.py
@@ -3,18 +3,56 @@
 
 cosine approximation function
 """
-__author__ = 'Qusai Al Shidi'
-__email__ = 'qusai@umich.edu'
+__author__ = 'Kaitlin Doublestein'
+__email__ = 'kdoubles@umich.edu'
 
 from math import factorial
 from math import pi
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 def cos_approx(x, accuracy=10):
+    """ Returns the approximation of cosine using the Taylor expansion form.
+    Args: 
+        x(float):
+            To evaluate cosine of.
+        accuracy (int):
+            (default: 10) Number of Taylor series coefficients to use.
+    
+    Returns:
+        (float):Approximate cosine of *x*.
+    
+    Examples:
+        from math import pi
+        cos_approx(pi)
+        cos_approx(pi,accuracy=50)
     """
-    """
-    return 
+    cos_approximation = sum([ ((-1)**n/factorial(2*n)) * (x**(2*n)) for n in range(accuracy) ])
+    return cos_approximation
 
+#Could rewrite so that the coefficients are separate from the series/
+#coefficient  = ((-1)**n/factorial(2*n))
+#series = (x**(2*n))
+#sum([(cofficient*series) for n in range(accuracy)])
+
+def cos_approx_sep(x,accuracy=10):
+    """Returns the approximation of cosine using the Taylor expansion form.
+    Args: 
+        x(float):
+            To evaluate cosine of.
+        accuracy (int):
+            (default: 10) Number of Taylor series coefficients to use.
+    
+    Returns:
+        (float):Approximate cosine of *x*.
+    
+    Examples:
+        from math import pi
+        cos_approx(pi)
+        cos_approx(pi,accuracy=50)
+    """
+    def coeff(n)
+    return 
 
 
 # Will only run if this is run from command line as opposed to imported
@@ -23,3 +61,13 @@ if __name__ == '__main__':  # main code block
     print("cos(pi) = ", cos_approx(pi))
     print("cos(2*pi) = ", cos_approx(2*pi))
     print("more accurate cos(2*pi) = ", cos_approx(2*pi, accuracy=50))
+    
+    
+
+x = np.linspace(0,1)
+plt.plot(x, np.exp(x))
+plt.xlabel(r'$0 \leq x < 1$')
+plt.ylabel(r'$e^x$')
+plt.title(r'Exponential Function')
+plt.show()
+
